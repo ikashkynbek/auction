6fb8271a30b705d8487c62e6f35ab21a97c3dc07
ik <ikashkynbek@gmail.com>
2017-01-09 16:34:01 +0600

    import sql

---
+++ data/import.sql
@@ -0,0 +1,2 @@
+INSERT INTO users (login, name, role, created, password) VALUES ('admin', 'Admin', 'ADMIN', now(),
+                                                                     'fef9adee173bf0aa1e3e72f7ed9dd1f2a8103a409d3c96de5febaf4d78e59b60963aedae14d54bde5a261d55e328786e463239ed47ba9eaba7d83014d099c7d6');
