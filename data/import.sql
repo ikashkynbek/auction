af6c757a186e70bc16798063148e1d7be470c5fd
dansharky <daniyar87kz@gmail.com>
2017-01-16 14:56:39 +0600

    added import of test products

--- data/import.sql
+++ data/import.sql
@@ -1,2 +1,6 @@
 INSERT INTO users (login, name, role, created, password) VALUES ('admin', 'Admin', 'ADMIN', now(),
                                                                      'fef9adee173bf0aa1e3e72f7ed9dd1f2a8103a409d3c96de5febaf4d78e59b60963aedae14d54bde5a261d55e328786e463239ed47ba9eaba7d83014d099c7d6');
+
+INSERT INTO PRODUCTS (name) VALUES ('LG G3');
+INSERT INTO PRODUCTS (name) VALUES ('iPhone 6');
+INSERT INTO PRODUCTS (name) VALUES ('NEXUS 5x');
